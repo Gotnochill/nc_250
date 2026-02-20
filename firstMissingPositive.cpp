a4cf53096d2d8832ced3174738841650bd430b04
Ritvik Mukherjee <mritvikcode1010@gmail.com>
2026-02-20 10:46:49 +0530

    few more mediums

---
+++ firstMissingPositive.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+
+            if (nums[i] !=  i+1){
+                swap(nums[i] , );
+            }
+        }
+    }
+};
